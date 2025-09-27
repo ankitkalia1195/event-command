42c3de29a4a295b39484fce6141cb8ab5c040c53
Ankit Kalia <ankit@opn.ooo>
2025-09-27 18:43:55 +0700

    Fix queue config

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -7,3 +7,6 @@ bin/rails assets:precompile
 bin/rails assets:clean
 
 bin/rails db:migrate
+
+# Setup SolidQueue tables
+bin/rails runner "ActiveRecord::Schema.define(version: 1) { load Rails.root.join('db', 'queue_schema.rb') }"
